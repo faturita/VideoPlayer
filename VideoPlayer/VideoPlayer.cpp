c256a0d8988485974db0d4d629d95c9239f46476
faturita <rramele@gmail.com>
2016-10-14 20:43:17 -0300

    Push files for the working project

---
+++ VideoPlayer/VideoPlayer.cpp
@@ -0,0 +1,72 @@
+// VideoPlayer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+	VideoCapture cap;
+	cap.open("tcp://192.168.1.1:5555/video?h264");
+	//cap.open("Drone.mov");
+
+
+	std::cout << "Using OpenCV version " << CV_VERSION << std::endl ;
+
+	std::cout << getBuildInformation();
+
+
+	if (!cap.isOpened()) 
+	{
+		cout << "Cant find the file." << endl;
+		return 0;
+	}
+
+	Mat edges;
+	namedWindow("edges",1);
+	for(;;)
+	{
+		Mat frame;
+		cap >> frame;
+		//frame = imread("e.1.l.1.c.9.tif", IMREAD_COLOR); // Read the file
+		imshow("edges", frame);
+		if (waitKey(30) >= 0) break;
+	}
+
+	return 0;
+}
+
+
+
+int mdain( int argc, char** argv )
+{
+    if( argc != 2)
+    {
+     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+     return -1;
+    }
+    Mat image;
+    image = imread(argv[1], IMREAD_COLOR); // Read the file
+    if( image.empty() ) // Check for invalid input
+    {
+        cout << "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
+    imshow( "Display window", image ); // Show our image inside it.
+    waitKey(0); // Wait for a keystroke in the window
+    return 0;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
